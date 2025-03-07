c29029d96320e0a87298171d7986d0b0be711718
Aditya <adityasahoo652@gmail.com>
2025-03-07 12:27:14 +0530

    feat: added esc button to exit and also handle the error throwing in close after clicking the close button

--- palette.py
+++ palette.py
@@ -9,10 +9,13 @@ cv2.createTrackbar("R","Colour Segmentation",0,255,nothing)
 cv2.createTrackbar("G","Colour Segmentation",0,255,nothing)
 cv2.createTrackbar("B","Colour Segmentation",0,255,nothing)
 while(True):
+    if cv2.getWindowProperty("Colour Segmentation", cv2.WND_PROP_VISIBLE) < 1:
+        break
     cv2.imshow('Colour Segmentation',img)
     r=cv2.getTrackbarPos("R","Colour Segmentation")
     g=cv2.getTrackbarPos("G","Colour Segmentation")
     b=cv2.getTrackbarPos("B","Colour Segmentation")
     img[:]=[b,g,r]
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
 cv2.destroyAllWindows()
