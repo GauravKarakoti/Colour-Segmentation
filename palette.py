e29b21f955c60b5a5693da80663e35a6250dd95f
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-03-10 21:50:46 +0530

    Change window name

--- palette.py
+++ palette.py
@@ -4,17 +4,17 @@ img=np.zeros((300,512,3),np.uint8)
 # frame=cv2.imread("image1.webp",-1)
 def nothing(x):
     return
-cv2.namedWindow("Colour Segmentation")
-cv2.createTrackbar("R","Colour Segmentation",0,255,nothing)
-cv2.createTrackbar("G","Colour Segmentation",0,255,nothing)
-cv2.createTrackbar("B","Colour Segmentation",0,255,nothing)
+cv2.namedWindow("Colour Palette")
+cv2.createTrackbar("R","Colour Palette",0,255,nothing)
+cv2.createTrackbar("G","Colour Palette",0,255,nothing)
+cv2.createTrackbar("B","Colour Palette",0,255,nothing)
 while(True):
-    if cv2.getWindowProperty("Colour Segmentation", cv2.WND_PROP_VISIBLE) < 1:
+    if cv2.getWindowProperty("Colour Palette", cv2.WND_PROP_VISIBLE) < 1:
         break
 
-    r=cv2.getTrackbarPos("R","Colour Segmentation")
-    g=cv2.getTrackbarPos("G","Colour Segmentation")
-    b=cv2.getTrackbarPos("B","Colour Segmentation")
+    r=cv2.getTrackbarPos("R","Colour Palette")
+    g=cv2.getTrackbarPos("G","Colour Palette")
+    b=cv2.getTrackbarPos("B","Colour Palette")
     img[:]=[b,g,r]
 
     color_img = np.full((300, 512, 3), (b, g, r), dtype=np.uint8)
@@ -25,7 +25,7 @@ while(True):
     text = f"R={r}, G={g}, B={b}"
     cv2.putText(img,text,(20,50),cv2.FONT_HERSHEY_SIMPLEX,1,text_color,2)
 
-    cv2.imshow("Colour Segmentation", img)
+    cv2.imshow("Colour Palette", img)
 
     key = cv2.waitKey(1) & 0xFF
     if key == 27:
